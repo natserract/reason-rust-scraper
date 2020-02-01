5a268a0ae38a0c49545a2416ca67f0f68cca816f
alfinsuryaS <benjaminstwo@gmail.com>
2020-02-01 09:55:15 +0800

    added new field (desc)

--- migrations/2020-01-31-113723_create_scrap/up.sql
+++ migrations/2020-01-31-113723_create_scrap/up.sql
@@ -3,6 +3,7 @@
 CREATE TABLE scrap (
   id INTEGER AUTO_INCREMENT PRIMARY KEY NOT NULL,
   site_name varchar(50) NOT NULL,
+  description varchar(100) NOT NULL,
   headers TEXT NOT NULL,
   body TEXT NOT NULL,
   created_at TIMESTAMP NULL,

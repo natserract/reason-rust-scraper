323cfba07b92d1cfde58cfd59b4884e17553e61e
alfinsuryaS <benjaminstwo@gmail.com>
2020-02-05 22:10:18 +0800

    Add new function result_opt()

--- server/src/utils.rs
+++ server/src/utils.rs
@@ -9,10 +9,19 @@ pub fn error_status() -> JsonValue {
     })
 }
 
-type ArgsType = Result<String, reqwest::Error>;
-pub fn result_res(args: ArgsType) -> String {
+pub type ArgsTypeRes = Result<String, reqwest::Error>;
+
+pub fn result_res(args: ArgsTypeRes) -> String {
     match args {
         Ok(result) => result,
         Err(err) => panic!("Error! Not Found: {}", err)
+    }
+}
+
+type ArgsTypeOption = Option<String>;
+pub fn result_opt(args: ArgsTypeRes) -> ArgsTypeOption {
+    match args {
+       Ok(result) => Some(result),
+       Err(_) => None
     }
 }
